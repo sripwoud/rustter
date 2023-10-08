ddcd133aab3d6894400d80a5b2e88ce792e8a35f
sripwoud <me@sripwoud.xyz>
2023-10-08 12:49:20 +0200

    define endpoint crate

---
+++ shared/endpoint/src/lib.rs
@@ -0,0 +1,9 @@
+pub mod user;
+
+pub trait Endpoint {
+    const URL: &'static str;
+
+    fn url(&self) -> &'static str {
+        Self::URL
+    }
+}
---
+++ shared/endpoint/src/user/endpoint.rs
@@ -0,0 +1,18 @@
+use serde::{Deserialize, Serialize};
+use rustter_domain::{ids::*, Password, Username};
+use crate::Endpoint;
+
+#[derive(Clone, Deserialize, Serialize)]
+pub struct CreateUser {
+    pub username: Username,
+    pub password: Password
+}
+impl Endpoint for CreateUser {
+    const URL:&'static str = "/account/create";
+}
+
+#[derive(Clone, Deserialize, Serialize)]
+pub struct CreateUserOk{
+    pub user_id:UserId,
+    pub username: Username
+}
