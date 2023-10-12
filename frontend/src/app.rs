b9a41e6b4d36ef7ac059454784450a7deee9c4d8
sripwoud <me@sripwoud.xyz>
2023-10-12 12:16:01 +0200

    revert use of render! instead of rsx!

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -7,7 +7,7 @@ use fermi::use_init_atom_root;
 pub fn App(cx: Scope) -> Element {
     use_init_atom_root(cx);
     // let _other_things = rsx! { p { "hi"} };
-    render! {
+    cx.render(rsx! {
         Router::<Route> {}
-    }
+    })
 }
