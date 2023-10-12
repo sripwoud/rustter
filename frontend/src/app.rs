e2c8481514235fa10f7fd64046195a8e1aa64eac
sripwoud <me@sripwoud.xyz>
2023-10-12 11:53:22 +0200

    bump fermi

--- frontend/src/app.rs
+++ frontend/src/app.rs
@@ -1,9 +1,10 @@
 #![allow(non_snake_case)]
+use fermi::use_init_atom_root;
 use crate::page::Route;
 use crate::prelude::*;
 
 pub fn App(cx: Scope) -> Element {
-    // use_init_atom_root(cx);
+    use_init_atom_root(cx);
     // let _other_things = rsx! { p { "hi"} };
     render! {
         Router::<Route> {}
