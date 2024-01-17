d2959c008ad8338fd77fafb42f47adf5550c5234
sripwoud <me@sripwoud.xyz>
2024-01-17 14:48:03 +0100

    fix: hide navbar on register and login pages (#30)

--- frontend/src/elements/navbar/NavBar.rs
+++ frontend/src/elements/navbar/NavBar.rs
@@ -5,6 +5,7 @@ use crate::elements::Sidebar;
 use crate::prelude::*;
 use dioxus::prelude::*;
 use dioxus_router::prelude::*;
+use rustter_endpoint::{CreateUser, Endpoint, Login};
 
 pub fn Init(cx: Scope) -> Element {
     let api_client = ApiClient::global();
@@ -40,17 +41,30 @@ pub fn Init(cx: Scope) -> Element {
 }
 
 pub fn NavBar(cx: Scope) -> Element {
+    let current_route = window().location().pathname().unwrap();
+    // let navigator = use_navigator(cx);
+    let hide_navbar = use_state(cx, || false);
     let new_post_popup_is_hidden = use_state(cx, || true);
     let hide_new_post_popup = move |_| {
         let is_hidden = *new_post_popup_is_hidden.get();
         new_post_popup_is_hidden.set(!is_hidden)
     };
 
+    use_effect(cx, (&current_route,), |(current_route,)| {
+        to_owned![hide_navbar];
+        async move {
+            let should_hide = current_route == Login::URL || current_route == CreateUser::URL;
+            hide_navbar.set(should_hide);
+        }
+    });
+
+    let hide_navbar_class = maybe_class!("hidden", *hide_navbar.get());
+
     cx.render(rsx! {
         Init {},
         Sidebar {},
         nav {
-            class: "max-w-[var(--content-max-width)] h-[var(--navbar-height)] fixed bottom-0 left-0 right-0 mx-auto border-t navbar-bg-color navbar-border-color",
+            class: "max-w-[var(--content-max-width)] h-[var(--navbar-height)] fixed bottom-0 left-0 right-0 mx-auto border-t navbar-bg-color navbar-border-color {hide_navbar_class}",
             div {
                 class: "grid grid-cols-3 justify-around w-full h-full items-center shadow-inner",
                 NavButton {
